b8ae759f8870e2b4bfcca7b4322ddf1e7bcf2dae
Iamneo <techadmin@iamneo.ai>
2023-08-02 08:58:47 +0000

    Backup Commit

--- reactapp/src/components/UI/Card/Card.js
+++ reactapp/src/components/UI/Card/Card.js
@@ -16,13 +16,18 @@ answer}) => {
                 {question}
             </h4>
             <Button onClick={disableBtn}
-            disabled = {state} >{options.option1}
+            disabled = {state} > {options.option1}
             </Button>
             <Button onClick={disableBtn}
             disabled= {state} > {options.option2}
             </Button>
             <Button onClick={disableBtn}
             disabled= {state} > {options.option3}
+            </Button>
+            <Button onClick={disableBtn}
+            disbaled= {state} > {options.option4}
+            </Button>
+            </div>
     )
-
 }
+export default Card;
