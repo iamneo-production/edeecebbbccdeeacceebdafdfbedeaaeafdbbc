1d02fedbcc4a8928bd55c2ca54335aedfdc9332f
Iamneo <techadmin@iamneo.ai>
2023-08-02 08:28:47 +0000

    Backup Commit

--- reactapp/src/App.js
+++ reactapp/src/App.js
@@ -1,3 +1,29 @@
 import {useState} from 'react';
 import './App.css';
-import Button from 
+import Button from './components/UI/Button/Button';
+import Card from './components/UI/Card/Card';
+import React from 'react';
+
+function App(){
+  var qCards;
+  var startBtn;
+  var resultBtn;
+
+  const questionData=[
+    {
+      key: 1,
+      question: 'Who is the father of the nation ?',
+      options: {
+        option1: 'Mahatma Gandhi',
+        option2: 'Jawaharlal Nehru',
+        option3: 'Donald Trump',
+        option4: 'Barrack Obama'
+      },
+      answer: 'Mahatma Gandhi'
+    },
+    {
+      key: 2,
+      question: 'What color is are the leaves ?',
+    }
+  ]
+}
